ad345972beaf50083157dfdfb48908db7551ded4
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-04 22:52:49 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[10] = {4,3,5,0,0,2};
+
+    cout << arr[0] << arr[1] << "." << arr[2] << arr[3] << arr[4];
+    
+    return 0;
+}
