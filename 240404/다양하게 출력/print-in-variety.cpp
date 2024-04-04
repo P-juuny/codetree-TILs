e4cf0bfab948044ae29c164359c66035fe18711f
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-04 22:26:11 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/다양하게 출력/print-in-variety.cpp
+++ 240404/다양하게 출력/print-in-variety.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
 
     cout << "Total days in Year" << endl;
-    cout << 365 << endl;
+    cout << "365" << endl;
     cout << "Circumference rate" << endl;
-    cout << 3.14159 << 26535;
+    cout << "3.1415926535";
     
     return 0;
 }
