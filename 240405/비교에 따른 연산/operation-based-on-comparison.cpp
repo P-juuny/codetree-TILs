53437c43a01945c64d8da9ab95cc31721b282278
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 12:03:50 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/비교에 따른 연산/operation-based-on-comparison.cpp
+++ 240405/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main() {
 
     int a, b;
+    int div = 0;
+
     cin >> a >> b;
 
     if(a > b){
         cout << a * b;
     }
     else{
-        cout << b / a - b % a;
+        cout << b / a;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
