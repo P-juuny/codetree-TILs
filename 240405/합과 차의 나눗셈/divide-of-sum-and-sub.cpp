3ce571eda41b521313727e4564923e0bec0284be
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 11:47:59 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+    float add = 0;
+    float sub = 0;
+    float div = 0;
+
+    cin >> a >> b;
+
+    add = a + b; //13
+    sub = a - b; // 7
+    div = add / sub;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << div;
+
+
+
+
+
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
