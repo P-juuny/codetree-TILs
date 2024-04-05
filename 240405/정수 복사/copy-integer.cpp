2d04e786565b86c8d2727a3707424b51bc376102
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 10:18:37 +0900

    Title: 정수 복사, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/정수 복사/copy-integer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 4;
+
+    b = a;
+
+    int mult = a * b;
+
+    cout << a << " " << b << endl;
+    cout << mult;
+
+    return 0;
+}
