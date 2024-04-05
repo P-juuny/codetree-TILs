66a22baf6be173711ab89c657a294df7d74a22d6
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 11:57:06 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+    int add = 0;
+    int u = 0;
+
+    cin >> a >> b >> c;
+
+    add = a + b + c;
+    u = add / 3;
+
+    cout << add << endl;
+    cout << u << endl;
+    cout << add - u; 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
