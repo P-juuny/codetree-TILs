b3ab4c23a5d08c24507e8e6a6198548df99a86d6
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 10:32:49 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int test_score;
+
+    cin >> test_score;
+
+    cout << "Your score is " << test_score << " point.";
+
+    return 0;
+}
