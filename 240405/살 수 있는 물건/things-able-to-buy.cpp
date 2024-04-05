2a16b9ed607a04a31ddfc2ebae873be5f9138fd6
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 12:28:55 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240405/살 수 있는 물건/things-able-to-buy.cpp
+++ 240405/살 수 있는 물건/things-able-to-buy.cpp
@@ -12,11 +12,11 @@ int main() {
     if(price >= 3000){
         cout << "book";
     }
-    else if(price == 0){
-        cout << "no";
+    else if(price < 3000){
+        cout << "mask";
     }
     else{
-        cout << "mask";
+        cout << "no";
     }
     // 여기에 코드를 작성해주세요.
     return 0;
