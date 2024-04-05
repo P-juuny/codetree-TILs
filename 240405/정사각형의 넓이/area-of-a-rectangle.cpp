5b077b4a5e4f07cf784ad2f2354bdbd4f7241997
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 11:58:34 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    
+    cin >> n;
+
+    if(n < 5){
+        cout << n * n << endl;
+        cout << "tiny";
+    }
+    else{
+        cout << n * n;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
