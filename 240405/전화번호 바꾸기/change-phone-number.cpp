c4631d10de8e2f65e8c901d32136fcaa459c4c73
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 11:08:58 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+    char d, e;
+
+    cin >> a >> d >> b >> e >> c;
+
+    cout << 0 << a << d << c << e << b;
+
+    return 0;
+}
