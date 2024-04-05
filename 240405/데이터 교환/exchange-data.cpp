2dbfab3fccf658690129f3bc571d45be3eab797e
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 10:24:14 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240405/데이터 교환/exchange-data.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp;
+    int temp_1;
+
+    temp = a;
+    temp_ 1 = b;
+    a = c;
+    b = temp;
+    c = temp_1;
+
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+
+    return 0;
+}
