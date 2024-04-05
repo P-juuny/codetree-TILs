4112e594293d2c6d66d856e4de5bd72b3d12b19b
P-juuny <164734769+P-juuny@users.noreply.github.com>
2024-04-05 12:08:55 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/삼항연산자 2/ternary-operator-2.cpp
+++ 240405/삼항연산자 2/ternary-operator-2.cpp
@@ -5,15 +5,13 @@ using namespace std;
 int main() {
     
     int a;
+    char ch;
 
     cin >> a;
     
-    if(a > 1 ? 't' : 'f'){
-        cout << 't';
-    }
-    else{
-        cout << 'f';
-    }
+    ch = (a == 1) ? 't' : 'f';
+
+    cout << ch;
 
     // 여기에 코드를 작성해주세요.
     return 0;
